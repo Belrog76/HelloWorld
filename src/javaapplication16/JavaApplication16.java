d04c08768236ce1850a500c088471399b59c9fa2
Belrog76 <xuanyi159753@gmail.com>
2020-02-22 13:38:20 -0500

    the first commit

---
+++ src/javaapplication16/JavaApplication16.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication16;
+
+/**
+ *
+ * @author kevin yang（杨轩易）
+ */
+public class JavaApplication16 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hellwo world");
+    }
+    
+    
+    
+}
